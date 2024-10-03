4a5254a67e69c40e87abfec38c1b1b319ca78b6a
Wilson Moura <wilo.moura3@gmail.com>
2024-10-02 22:12:41 -0300

    Melhoria na forma que costroi o retorno da requisição

--- src/core/rest/post_login.rs
+++ src/core/rest/post_login.rs
@@ -2,6 +2,7 @@ use rocket::State;
 use rocket::{post, http::Status};
 use rocket::serde::json::Json;
 use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
 
 use crate::adapters::database::db::DbPool;
 use crate::core::repository::find_user_by_username;
@@ -17,13 +18,7 @@ pub struct LoginRequest {
 #[derive(Serialize)]
 pub struct LoginResponse {
     pub token: String,
-    pub account: UserPresenter
-}
-
-#[derive(Serialize)]
-pub struct UserPresenter {
-    pub email: String,
-    pub name: String,
+    pub user: Value
 }
 
 #[post("/login", data = "<login>")]
@@ -34,12 +29,13 @@ pub fn action(login: Json<LoginRequest>, db: &State<DbPool>) -> Result<Json<Logi
         if bcrypt::verify(&login.password, &user.password_hash).unwrap() {
             let token = generate_token(user.id);
 
-            let user_presenter = UserPresenter {
-                email: user.username,
-                name: user.name
-            };
-
-            return Ok(Json(LoginResponse { token, account: user_presenter }));
+            return Ok(Json(LoginResponse { 
+                token, 
+                user: json!({
+                    "email": user.username,
+                    "name": user.name
+                })
+            }));
         }
     }
 
